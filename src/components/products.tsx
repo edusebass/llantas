288f5a279ffeb9cfe108c47d39bd36c611fab43a
Willians Jimenez <williansaaid@hotmail.com>
2023-09-29 21:10:46 -0500

    Map

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,33 +1,49 @@
 import { useState, useEffect } from 'react'
+import Card from './Card';
 
-const json = "llantas.json"
+const json = "llantas.json";
 
 const products = () => {
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState();
 
     useEffect(() => {
+        // Use the fetch API to load the JSON data
+        fetch(json)
+            .then((response) => response.json())
+            .then((jsonData) => {
+                // Set the JSON data to your component state
+                setData(jsonData.rines);
+            })
+            .catch((error) => {
+                console.error('Error loading JSON data:', error);
+            });
 
-    // Use the fetch API to load the JSON data
-    fetch(json)
-        .then((response) => response.json())
-        .then((jsonData) => {
-            // Set the JSON data to your component state
-            setData(jsonData);
-        })
-        .catch((error) => {
-            console.error('Error loading JSON data:', error);
-        });
     }, []);
 
+    console.log(data);
+
     return (
-
-    <section className=" bg-gray-100 flex">
-        <div className="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {data}
-        </div>
-    </section>
-
+        <section className=" bg-gray-100 flex">
+            <div
+                className="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+            >
+                {
+                    data !== undefined ?
+                    data.map((llanta) => {
+                        <Card
+                            ancho={llanta.ancho}
+                            altura={llanta.altura}
+                            rin={llanta.rin}
+                            marcaimg={llanta.marcaimg}
+                            precio={llanta.precio}
+                            foto={llanta.foto}
+                        />
+                    })
+                    : <p>Loading...</p>
+                }
+            </div>
+        </section>
     );
 };
 
