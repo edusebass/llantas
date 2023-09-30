9d6764b9869df75ace8ae51c04a68b26eaa0be40
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-09-29 21:39:41 -0500

    bug fixed vercel

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,37 +1,38 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
+
+interface Llanta {
+    ancho: number;
+    altura: number;
+    rin: number;
+    marcaimg: string;
+    precio: number;
+    foto: string;
+}
 
 const json = "llantas.json";
 
-const products = () => {
-
-    const [data, setData] = useState(null);
+const Products: React.FC = () => {
+    const [data, setData] = useState<Llanta[] | null>(null);
 
     useEffect(() => {
-        // Use the fetch API to load the JSON data
         fetch(json)
             .then((response) => response.json())
-            .then((jsonData) => {
-                // Set the JSON data to your component state
+            .then((jsonData: { rines: Llanta[] }) => {
                 setData(jsonData.rines);
             })
             .catch((error) => {
                 console.error('Error loading JSON data:', error);
             });
-
     }, []);
 
-    console.log(data);
-
     return (
-        <section className=" bg-gray-100 flex">
-            <div
-                className="mx-auto w-full h-full grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
-            >
-                {
-                    data !== null ?
-                    data.map((llanta: any) => {
+        <section className="bg-gray-100 flex">
+            <div className="mx-auto w-full h-full grid max-w-6xl grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {data !== null ? (
+                    data.map((llanta, index) => (
                         <Card
+                            key={index}
                             ancho={llanta.ancho}
                             altura={llanta.altura}
                             rin={llanta.rin}
@@ -39,12 +40,13 @@ const products = () => {
                             precio={llanta.precio}
                             foto={llanta.foto}
                         />
-                    })
-                    : <p>Loading...</p>
-                }
+                    ))
+                ) : (
+                    <p>Loading...</p>
+                )}
             </div>
         </section>
     );
 };
 
-export default products;
+export default Products;
