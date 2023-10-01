57b07b4606fe98c0207343e0942a8984a2086781
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-01 18:36:03 -0500

    link whatsapp

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,7 @@ interface Props {
     foto: string;
 }
 
+
 function Card({
     ancho,
     altura,
@@ -44,7 +45,11 @@ function Card({
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="h-4 w-4">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                             </svg>
-                            <button className="text-md uppercase font-bold">Cotizar</button>
+                            <a href={`https://wa.me/+5930985661198?text=Me%20interesa%20la%20medida%20${ancho}/${altura}R${rin}`} target="_blank">
+                                <button className="text-md uppercase font-bold">
+                                Cotizar
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
