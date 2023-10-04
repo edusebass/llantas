0d946b9ece7596e51ba2ce636bed3bd730962914
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-04 11:41:43 -0500

    (labrado, marca) added card  and link whatsat

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,9 @@ function Card({
     rin,
     marcaimg,
     precio,
-    foto
+    foto,
+    labrado,
+    marca
 }: Llanta) {
     return (
         <article
@@ -22,7 +24,7 @@ function Card({
                 </div>
                 <div className="flex flex-col items-center justify-center gap-4">
                     <h2 className="text-slate-700 font-bold text-xl">
-                        {`${ancho}/${altura}R${rin}`}
+                        {`${ancho}/${altura}R${rin} ${labrado} ${marca}`}
                     </h2>
                     <img src={marcaimg} alt="logo marca"
                         className="h-8 object-cover object-center w-auto"
@@ -39,7 +41,7 @@ function Card({
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
                             </svg>
                             <a
-                                href={`https://wa.me/+5930985661198?text=¡Saludos!,%20Me%20interesa%20la%20medida%20${ancho}/${altura}R${rin}`}
+                                href={`https://wa.me/+5930985661198?text=¡Saludos!,%20Me%20interesa%20la%20medida%20${ancho}/${altura}R${rin} ${labrado} ${marca}`}
                                 target="_blank"
                             >
                                 <button
