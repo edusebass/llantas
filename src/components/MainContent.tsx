e8a20e486c5de55d5838244329aa37172128cae8
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-02-16 10:44:08 -0500

    add changes

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -3,38 +3,38 @@ const MainContent = () => {
         <>
             <main className="my-8">
                 <div className="container mx-auto px-6">
-                    <div className="h-64 rounded-md overflow-hidden bg-cover bg-center" >
+                    <div className="h-64 rounded-md overflow-hidden bg-cover bg-center" style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1509042009371-65fa830819f1?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bGxhbnRhJTIwZmVycmFyaXxlbnwwfHwwfHx8MA%3D%3D)' }}>
                         <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
                             <div className="px-10 max-w-xl">
-                                <h2 className="text-2xl text-white font-semibold">Sport Shoes</h2>
+                                <h2 className="text-2xl text-white font-semibold">Auto</h2>
                                 <p className="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
                                 <button className="flex items-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-                                    <span>Shop Now</span>
+                                    <span>Cotiza tu medida</span>
                                     <svg className="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
                                 </button>
                             </div>
                         </div>
                     </div>
                     <div className="md:flex mt-8 md:-mx-4">
-                        <div className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:w-1/2" >
+                        <div className="w-full h-64 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:w-1/2" style={{ backgroundImage: 'url(https://www.motortrend.com/uploads/2021/10/2022-Genesis-GV70-3-5T-AWD-38.jpg?fit=around%7C784:441)' }}>
                             <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
                                 <div className="px-10 max-w-xl">
-                                    <h2 className="text-2xl text-white font-semibold">Back Pack</h2>
+                                    <h2 className="text-2xl text-white font-semibold">Suv</h2>
                                     <p className="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
                                     <button className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none">
-                                        <span>Shop Now</span>
+                                        <span>Cotiza tu medida</span>
                                         <svg className="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
                                     </button>
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2" >
+                        <div className="w-full h-64 mt-8 md:mx-4 rounded-md overflow-hidden bg-cover bg-center md:mt-0 md:w-1/2"  style={{ backgroundImage: 'url(https://media.chevroletarabia.com/dld/content/Pages/news/co/es/2011/Aug/0810_camiones/_jcr_content/rightpar/sectioncontainer_1/par/download/file.res/Chevrolet%20SERIE%20F%20FONDO.jpg)' }} >
                             <div className="bg-gray-900 bg-opacity-50 flex items-center h-full">
                                 <div className="px-10 max-w-xl">
-                                    <h2 className="text-2xl text-white font-semibold">Games</h2>
+                                    <h2 className="text-2xl text-white font-semibold">Camion</h2>
                                     <p className="mt-2 text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident molestias ipsam sint voluptatum pariatur.</p>
                                     <button className="flex items-center mt-4 text-white text-sm uppercase font-medium rounded hover:underline focus:outline-none">
-                                        <span>Shop Now</span>
+                                        <span>Cotiza tu medida</span>
                                         <svg className="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
                                     </button>
                                 </div>
