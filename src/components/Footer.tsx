ea8fab79fb203045521592c623fac18548e0211d
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2024-02-16 11:05:11 -0500

    add fecha footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
         <>
             <footer className="bg-gray-200">
                 <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-                    <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">Brand</a>
-                    <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+                    <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">Llantera Quitumbe</a>
+                    <p className="py-2 text-gray-500 sm:py-0">{new Date().getFullYear()} All rights reserved</p>
                 </div>
             </footer>
         </>
